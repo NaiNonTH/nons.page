b13c8828d5408f0fb0222af01f2af2ee6cac68da
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-05-13 23:21:05 +0700

    Remove Tritanopia links setting due to the addition of themes

--- src/assets/scripts/settings.js
+++ src/assets/scripts/settings.js
@@ -3,20 +3,6 @@
         preferences = preferences ? JSON.parse(preferences) : {};
 
     const html = document.documentElement;
-    const tritanopia = document.getElementById("tritanopia-links");
-
-    if (preferences.tritanopia) tritanopia.checked = true;
-    
-    tritanopia.onchange = () => {
-        if (tritanopia.checked) {
-            html.setAttribute("tritanopia", "")
-            setPreference("tritanopia", true);
-        }
-        else {
-            html.removeAttribute("tritanopia");
-            setPreference("tritanopia", false);
-        }
-    };
 
     const themes = document.getElementsByName("themes");
     const currentTheme = preferences.theme || "broken-hoe";
