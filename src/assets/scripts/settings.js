176617850e094390016bf14468174063c63e087e
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-06-13 18:06:41 +0700

    Add Auto theme setting

--- src/assets/scripts/settings.js
+++ src/assets/scripts/settings.js
@@ -5,12 +5,7 @@
     const html = document.documentElement;
 
     const themes = document.getElementsByName("themes");
-    const currentTheme = 
-        preferences.theme || (
-            window.matchMedia("(prefers-color-scheme: dark)").matches
-            ? "bedtime"
-            : "broken-hoe"
-        );
+    const currentTheme = preferences.theme || "auto";
 
     themes.forEach((theme) => {
         if (currentTheme === theme.value) {
