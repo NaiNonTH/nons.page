2cc76dcb98f8b2d90e5720ee079a9dc53c590b2b
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-06-26 23:06:19 +0700

    Add file last modified stat for article layout + related changes

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ const mdImplicitFigures = require("markdown-it-image-figures");
 const pluginRss = require("@11ty/eleventy-plugin-rss");
 const CleanCSS = require("clean-css");
 const mdAnchor = require("markdown-it-anchor");
+const { statSync } = require("fs");
 
 function minifyjs(inputContent) {
     const minified = uglifyjs.minify(inputContent, {
@@ -15,6 +16,23 @@ function minifyjs(inputContent) {
     });
 
     return minified;
+}
+function toDateString(dateFormat) {
+    const months = [
+        "January", "February", "March",
+        "April", "May", "June", "July",
+        "August", "September", "October",
+        "November", "December"
+    ];
+    const date = dateFormat.getDate();
+    const dateSuffixes = ["st", "nd", "rd"];
+    const dateSuffix =
+        ([1, 2, 3].includes(date % 10)) &&
+        (Math.trunc(date / 10) !== 1)
+            ? dateSuffixes[(date % 10) - 1]
+            : "th";
+
+    return `${date}${dateSuffix} ${months[dateFormat.getMonth()]}, ${dateFormat.getFullYear()}`;
 }
 
 module.exports = function(config) {
@@ -35,29 +53,19 @@ module.exports = function(config) {
         async: true
     }));
 
+    config.addShortcode("lastModifiedDate", function(src) {
+        const { mtime } = statSync(src);
+        const dateString = toDateString(mtime);
+        
+        return `${dateString} (${mtime.getUTCHours().toString().padStart(2, "0")}:${mtime.getUTCMinutes().toString().padStart(2, "0")} UTC)`;
+    });
     config.addFilter("cacheBust", function(url) {
         return url + `?t=${Date.now()}`;
-    })
+    });
     config.addFilter("formatDateSlug", function(dateFormat) {
         return `${dateFormat.getFullYear()}-${dateFormat.getMonth().toString().padStart(2, "0")}-${dateFormat.getDate().toString().padStart(2, "0")}`
     });
-    config.addFilter("formatDate", function(dateFormat) {
-        const months = [
-            "January", "February", "March",
-            "April", "May", "June", "July",
-            "August", "September", "October",
-            "November", "December"
-        ];
-        const date = dateFormat.getDate();
-        const dateSuffixes = ["st", "nd", "rd"];
-        const dateSuffix =
-            ([1, 2, 3].includes(date % 10)) &&
-            (Math.trunc(date / 10) !== 1)
-                ? dateSuffixes[(date % 10) - 1]
-                : "th";
-
-        return `${date}${dateSuffix} ${months[dateFormat.getMonth()]}, ${dateFormat.getFullYear()}`
-    });
+    config.addFilter("formatDate", toDateString);
 
     config.addTemplateFormats("scss");
     config.addExtension("scss", {
