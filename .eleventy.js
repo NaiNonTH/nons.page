f342b4183979aa1bdd990ffc4a82213aa251838f
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-07-02 22:01:21 +0700

    Explicit use the assets dir's  own way to copy images

--- .eleventy.js
+++ .eleventy.js
@@ -137,7 +137,7 @@ module.exports = function(config) {
     });
 
     config.addPassthroughCopy({ "src/_assets/images": "assets/images" });
-    config.addPassthroughCopy("src/[!_]*/**/*.{jpg,webp,svg,png,gif}");
+    config.addPassthroughCopy("src/[!_assets]*?/**/*.{jpg,webp,svg,png,gif}");
     config.addPassthroughCopy("src/*.{jpg,webp,svg,png,gif}");
 
     return {
